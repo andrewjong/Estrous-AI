5809aa3fb8c42ae2d37944d308ce7cb7c87b56e6
Andrew Jong <andrewjong87@gmail.com>
2018-08-13 19:59:32 -0700

    Progress

--- train.py
+++ train.py
@@ -12,15 +12,18 @@ from src.model_choices import TRAIN_MODEL_CHOICES
 
 TRAIN_RESULTS_FNAME = "train.csv"
 
-def build_and_train_model():
+
+def build_and_train_model(model_starter_file=False):
     """ This is the main training function that constructs the model and trains
     it on the chosen dataset.
     """
     # Obtain train and validation datasets and dataloaders
     datasets, dataloaders = utils.get_datasets_and_loaders(
         args.data_dir, "train", "val", batch_size=args.batch_size)
-    dataset_sizes = {subset: len(datasets[subset])
-                     for subset in ('train', 'val')}
+    dataset_sizes = {
+        subset: len(datasets[subset])
+        for subset in ('train', 'val')
+    }
 
     num_classes = len(datasets["train"].classes)
 
@@ -28,6 +31,11 @@ def build_and_train_model():
     TrainableClass = TRAIN_MODEL_CHOICES[args.model]
     try:
         trainable = TrainableClass(num_classes, *args.added_args)
+        # load in existing weights if requested
+        if model_starter_file:
+            trainable.model.load_state_dict(
+                torch.load(
+                    model_starter_file, map_location=lambda storage, loc: storage))
     except TypeError as e:
         print("Caught TypeError when instantiating model class. Make sure " +
               "all required model arguments are passed, in order, using the " +
@@ -103,41 +111,64 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Train an estrous cycle cell-phase classifer model.")
 
-    parser.add_argument("model",
-                        choices=list(TRAIN_MODEL_CHOICES),
-                        nargs="?",
-                        help=f'Choose which model to use.')
-    parser.add_argument("-e", "--experiment_name",
-                        default='unnamed_experiment',
-                        help='Name of the experiment. This becomes the \
+    parser.add_argument(
+        "model",
+        choices=list(TRAIN_MODEL_CHOICES),
+        nargs="?",
+        help=f'Choose which model to use.')
+    parser.add_argument(
+        "-e",
+        "--experiment_name",
+        default='unnamed_experiment',
+        help='Name of the experiment. This becomes the \
                         subdirectory that the experiment output is stored in, \
                         i.e. "experiments/my_experiment/" (default: \
                         "unnamed_experiment").')
-    parser.add_argument("-d", "--data_dir",
-                        help='Root directory of dataset to use, with classes \
+    parser.add_argument(
+        "-d",
+        "--data_dir",
+        help='Root directory of dataset to use, with classes \
                         separated into separate subdirectories.')
-    parser.add_argument("-n", "--num_epochs", type=int, metavar="N",
-                        default=50,
-                        help='Number of epochs to train for (default: 50).')
-    parser.add_argument("-b", "--batch_size", default=4, type=int,
-                        help="Select a batch size.")
-    parser.add_argument("-a", "--added_args", nargs="+",
-                        default=[],
-                        help="Pass addiitional arguments for instantiating the \
+    parser.add_argument(
+        "-n",
+        "--num_epochs",
+        type=int,
+        metavar="N",
+        default=50,
+        help='Number of epochs to train for (default: 50).')
+    parser.add_argument(
+        "-b", "--batch_size", default=4, type=int, help="Select a batch size.")
+    parser.add_argument(
+        "-a",
+        "--added_args",
+        nargs="+",
+        default=[],
+        help="Pass addiitional arguments for instantiating the \
                         chosen model.")
-    parser.add_argument("-l", "--load_args",
-                        help="Option to load in train args from a previous \
+    parser.add_argument(
+        "-l",
+        "--load_args",
+        help="Option to load in train args from a previous \
                         train session using that session's \"meta.json\" \
                         file. Passed and default args will override loaded \
                         args.")
-    parser.add_argument("--skip_metrics",
-                         action='store_true',
-                         help="Choose to NOT run predict.py and metrics.py \
+    parser.add_argument(
+        "-p",
+        "--use_previous_model",
+        action='store_true',
+        help="Continue training from the model.pth file \
+                        created from a previous train run. Must have \
+                        --load_args argument passed in.")
+    parser.add_argument(
+        "--skip_metrics",
+        action='store_true',
+        help="Choose to NOT run predict.py and metrics.py \
                          on the validation-set after training, i.e. do not \
                          calculate performance metrics.")
-    parser.add_argument("--overwrite", 
-                        action='store_true', 
-                        help="Skip the prompt that double checks whether to \
+    parser.add_argument(
+        "--overwrite",
+        action='store_true',
+        help="Skip the prompt that double checks whether to \
                         overwrite existing files. Existing files will be \
                         overwritten.")
 
@@ -145,7 +176,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print()
-    print(f'*** BEGINNING EXPERIMENT: "{args.experiment_name}" ***', end="\n\n")
+    print(
+        f'*** BEGINNING EXPERIMENT: "{args.experiment_name}" ***', end="\n\n")
 
     # load args from a previous train session if requested
     if args.load_args:
@@ -178,11 +210,17 @@ if __name__ == '__main__':
     dataset_name = os.path.basename(args.data_dir)
     # Make the output directory for the experiment
     global outdir
-    outdir = os.path.join(EXPERIMENTS_ROOT, args.experiment_name,
-                          dataset_name, model_name)
+    outdir = os.path.join(EXPERIMENTS_ROOT, args.experiment_name, dataset_name,
+                          model_name)
+
+    if args.use_previous_model:
+        prev_model_file = os.path.join(args.load_args, "model.pth")
+        print("Loading previous model from ", prev_model_file)
+    else:
+        prev_model_file = False
 
     # run the main training script
-    build_and_train_model()
+    build_and_train_model(prev_model_file)
 
     # calculate performance metrics with the saved model
     # print('skip_metrics: ', args.skip_metrics)
